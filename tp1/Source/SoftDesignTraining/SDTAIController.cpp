87e6a575bc6c3ae6b6f1198c1c7147a44f143954
Salim Eid <saloums7@gmail.com>
2019-01-28 18:09:14 -0500

    Implement Movement & Object detection
    Pawn moves towards collectible. Doesn't matter which one, it goes straight to it. No wall avoidance. YOLO

--- tp1/Source/SoftDesignTraining/SDTAIController.cpp
+++ tp1/Source/SoftDesignTraining/SDTAIController.cpp
@@ -2,12 +2,58 @@
 
 #include "SoftDesignTraining.h"
 #include "SDTAIController.h"
+#include "DrawDebugHelpers.h"
+#include "PhysicsHelpers.h"
+#include "SDTCollectible.h"
 
 void ASDTAIController::Tick(float deltaTime)
 {
+	auto world = GetWorld();
+	auto physicsHelper = PhysicsHelpers(world);
 
+	DrawVisionCone(world, GetPawn(), GetPawn()->GetActorForwardVector(), m_visionAngle);
+	TArray<FOverlapResult> detectedItems = CollectTargetActorsInFrontOfCharacter(GetPawn(), physicsHelper);
+
+	for (FOverlapResult item : detectedItems)
+	{
+		if(item.GetActor()->IsA(ASDTCollectible::StaticClass()))
+		{
+			FVector2D collectible = FVector2D(item.GetActor()->GetActorLocation());
+
+			MoveToTarget(collectible, m_maxSpeed, deltaTime);
+		}
+	}
 }
 
+bool ASDTAIController::MoveToTarget(FVector2D target, float targetSpeed, float deltaTime)
+{
+	APawn* pawn = GetPawn();
+	FVector const pawnPosition(pawn->GetActorLocation());
+	FVector2D const toTarget = target - FVector2D(pawnPosition);
+	FVector2D const displacement = FMath::Min(toTarget.Size(), FMath::Min(m_maxSpeed, targetSpeed) * deltaTime) * toTarget.GetSafeNormal();
+	pawn->SetActorLocation(pawnPosition + FVector(displacement, 0.f), true);
+	pawn->SetActorRotation(FVector(displacement, 0.f).ToOrientationQuat());
+	return toTarget.Size() < FMath::Min(m_maxSpeed, targetSpeed) * deltaTime;
+}
 
+void ASDTAIController::MoveTowardsDirection(FVector2D direction, float speed, float deltaTime)
+{
+	APawn* pawn = GetPawn();
+	FVector const pawnPosition(pawn->GetActorLocation());
+	FVector2D const displacement = FMath::Min(m_maxSpeed, speed) * deltaTime * direction.GetSafeNormal();
+	
+	pawn->SetActorLocation(pawnPosition + FVector(displacement, 0.f), true);
+	pawn->SetActorRotation(FVector(direction, 0.f).ToOrientationQuat());
+}
 
+void ASDTAIController::DrawVisionCone(UWorld* world, APawn* pawn, FVector const& dir, float angle) const
+{
+	DrawDebugCone(world, pawn->GetActorLocation(), dir, 5000.0f, angle, angle, 50, FColor::Green, false, -1.0f, 0, 5.0f);
+}
 
+TArray<FOverlapResult> ASDTAIController::CollectTargetActorsInFrontOfCharacter(APawn const* pawn, PhysicsHelpers& physicHelper) const
+{
+	TArray<FOverlapResult> outResults;
+	physicHelper.SphereOverlap(pawn->GetActorLocation() + pawn->GetActorForwardVector() * 750.0f, 750.0f, outResults, true);
+	return outResults;
+}
--- tp1/Source/SoftDesignTraining/SDTAIController.h
+++ tp1/Source/SoftDesignTraining/SDTAIController.h
@@ -7,6 +7,9 @@
 
 #include "SDTAIController.generated.h"
 
+
+class PhysicsHelpers;
+
 /**
  * 
  */
@@ -16,4 +19,16 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
     GENERATED_BODY()
 public:
     virtual void Tick(float deltaTime) override;
+
+protected:
+	virtual bool MoveToTarget(FVector2D target, float speed, float deltaTime);
+	virtual void MoveTowardsDirection(FVector2D direction, float speed, float deltaTime);
+
+	virtual void DrawVisionCone(UWorld* world, APawn* pawn, FVector const& dir, float angle) const;
+	virtual TArray<FOverlapResult> CollectTargetActorsInFrontOfCharacter(APawn const* pawn, PhysicsHelpers& physicHelper) const;
+
+
+	FVector2D m_MovementInput;
+	float const m_maxSpeed = 500.0f;
+	float const m_visionAngle = PI / 3.0f;
 };
